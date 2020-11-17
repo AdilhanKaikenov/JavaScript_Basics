292cbb174f0325c4dca230a32da57d22abbe43f4
Kaikenov Adilhan <adilhan_kai@mail.ru>
2020-11-17 13:04:08 +0600

    first basic lesson

--- script.js
+++ script.js
@@ -41,8 +41,8 @@ const   a = prompt("Один из последних просмотренных 
         b = prompt("На сколько оцените его?", ""),
         c = prompt("Один из последних просмотренных фильмов?", ""),
         d = prompt("На сколько оцените его?", "");
-        
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
