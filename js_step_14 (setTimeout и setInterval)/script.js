cd62500516e0638edc8a0b8aecec70a5514171c1
Kaikenov Adilhan <adilhan_kai@mail.ru>
2020-11-19 14:54:09 +0600

    js_step_14 (setTimeout и setInterval)

---
+++ js_step_14 (setTimeout и setInterval)/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const btn = document.querySelector('.btn');
+let timerId;
+
+function myAnimation() {
+    const box = document.querySelector('.box');
+    let pos = 0;
+
+    const id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            box.style.top = pos + 'px';
+            box.style.left = pos + 'px';
+        }
+    }
+}
+
+btn.addEventListener('click', myAnimation);
