d1a63b86fff9187cb892d00920ff3be0485b0628
Kaikenov Adilhan <adilhan_kai@mail.ru>
2020-11-24 10:23:34 +0600

    requestAnimationFrame

--- js_step_14 (setTimeout и setInterval)/script.js
+++ js_step_14 (setTimeout и setInterval)/script.js
@@ -1,21 +1,36 @@
 'use strict';
 
-const btn = document.querySelector('.btn');
+const   btn = document.querySelector('.btn'),
+        box = document.querySelector('.box');
+let pos = 0;
+
+// 1)
+
+// function myAnimation() {
+//     const id = setInterval(frame, 10);
+//     function frame() {
+//         if (pos == 300) {
+//             clearInterval(id);
+//         } else {
+//             pos++;
+//             box.style.top = pos + 'px';
+//             box.style.left = pos + 'px';
+//         }
+//     }
+// }
+
+// btn.addEventListener('click', myAnimation);
+
+// 2)
 
 function myAnimation() {
-    const box = document.querySelector('.box');
-    let pos = 0;
+    pos++;
+    box.style.top = pos + 'px';
+    box.style.left = pos + 'px';
 
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 300) {
-            clearInterval(id);
-        } else {
-            pos++;
-            box.style.top = pos + 'px';
-            box.style.left = pos + 'px';
-        }
+    if (pos < 300) {
+        requestAnimationFrame(myAnimation);
     }
 }
 
-btn.addEventListener('click', myAnimation);
+btn.addEventListener('click', () => requestAnimationFrame(myAnimation));
